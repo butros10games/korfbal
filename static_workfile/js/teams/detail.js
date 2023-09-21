b5f67bd3cdce463e77c626aab8fc787c8e21f640
Butros Groot MacBook <butrosgroot@gmail.com>
2023-09-21 22:27:39 +0200

    # Update - Added the js dom manipulation to display the goal information

--- static_workfile/js/teams/detail.js
+++ static_workfile/js/teams/detail.js
@@ -181,8 +181,96 @@ function updateMatches(data) {
 }
 
 function updateGoalStats(data) {
-    if (data.goal_stats.length > 0) {
-        console.log(data.goal_stats);
+    if (data.played_matches > 0) {
+        goals_container = document.createElement("div");
+        goals_container.classList.add("flex-column");
+        goals_container.style.width = "calc(100% - 24px))";
+        goals_container.style.padding = "12px";
+
+        row_1 = document.createElement("div");
+        row_1.classList.add("flex-row");
+        row_1.style.justifyContent = "space-around";
+        row_1.style.width = "100%";
+        row_1.style.marginBottom = "24px";
+        
+        matchs_container = document.createElement("div");
+        matchs_container.classList.add("flex-column");
+        matchs_container.style.width = "144px";
+
+        matchs = document.createElement("p");
+        matchs.style.margin = "0";
+        matchs.style.fontSize = "14px";
+        matchs.innerHTML = "Wedstrijden";
+
+        matchs_data = document.createElement("p");
+        matchs_data.style.margin = "0";
+        matchs_data.innerHTML = data.played_matches;
+
+        matchs_container.appendChild(matchs);
+        matchs_container.appendChild(matchs_data);
+
+        row_1.appendChild(matchs_container);
+
+        total_score_container = document.createElement("div");
+        total_score_container.classList.add("flex-column");
+        total_score_container.style.width = "144px";
+
+        total_score = document.createElement("p");
+        total_score.style.margin = "0";
+        total_score.style.fontSize = "14px";
+        total_score.innerHTML = "Totaal punten";
+
+        total_score_data = document.createElement("p");
+        total_score_data.style.margin = "0";
+        total_score_data.innerHTML = data.total_goals_for + '/' + data.total_goals_against;
+
+        total_score_container.appendChild(total_score);
+        total_score_container.appendChild(total_score_data);
+
+        row_1.appendChild(total_score_container);
+
+        goals_container.appendChild(row_1);
+
+        row_2 = document.createElement("div");
+
+        // Create a container for goal stats per type
+        goal_stats_container = document.createElement("div");
+        goal_stats_container.classList.add("flex-row");
+        goal_stats_container.style.width = "100%";
+        goal_stats_container.style.marginTop = "12px";
+        goal_stats_container.style.flexWrap = "wrap";
+        goal_stats_container.style.justifyContent = "space-around";
+
+        // Iterate through goal_stats object
+        for (const goalType in data.goal_stats) {
+            if (data.goal_stats.hasOwnProperty(goalType)) {
+                const goalStat = data.goal_stats[goalType];
+
+                // Create a div for each goal type's stats
+                goal_type_container = document.createElement("div");
+                goal_type_container.classList.add("flex-column");
+                goal_type_container.style.marginbottom = "12px";
+                goal_type_container.style.width = "104px";
+                goal_type_container.style.marginBottom = "12px";
+
+                goal_type_name = document.createElement("p");
+                goal_type_name.style.margin = "0";
+                goal_type_name.style.fontSize = "14px";
+                goal_type_name.innerHTML = goalType;
+
+                goals_data = document.createElement("p");
+                goals_data.style.margin = "0";
+                goals_data.innerHTML = goalStat.goals_for + "/" + goalStat.goals_against;
+
+                goal_type_container.appendChild(goal_type_name);
+                goal_type_container.appendChild(goals_data);
+
+                goal_stats_container.appendChild(goal_type_container);
+            }
+        }
+
+        goals_container.appendChild(goal_stats_container);
+        infoContainer.appendChild(goals_container);
     } else {
         infoContainer.classList.add("flex-center");
         infoContainer.innerHTML = "<p style='text-align: center;'>Er zijn nog geen doelpunten gemaakt</p>";
@@ -192,7 +280,10 @@ function updateGoalStats(data) {
 function updatePlayers(data) {
     if (data.spelers.length > 0) {
         for (i = 0; i < data.spelers.length; i++) {
-            player_container = document.createElement("div");
+            player_container = document.createElement("a");
+            player_container.href = "/spelers/" + data.spelers[i].id + "/";
+            player_container.style.textDecoration = "none";
+            player_container.style.color = "#000";
             player_container.classList.add("player-container");
 
             player_profile_pic = document.createElement("img");
