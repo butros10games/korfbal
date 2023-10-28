47674469d247f3f0fd2376d1d85b6419e293681e
Butros Groot Desktop <butrosgroot@gmail.com>
2023-10-28 22:14:09 +0200

    # Update - Added the switch button - Added the for or against score button

--- static_workfile/js/matches/tracker.js
+++ static_workfile/js/matches/tracker.js
@@ -26,19 +26,95 @@ window.addEventListener("DOMContentLoaded", function() {
     load_icon(playersDiv);
     initializeSocket(WebSocket_url);
     initializeButtons();
+    scoringButtonSetup();
 });
 
 function initializeButtons() {
     const buttons = document.getElementsByClassName("button");
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener("click", function() {
+    for (const element of buttons) {
+        element.addEventListener("click", function() {
             const data = {
                 "command": "event",
-                "event": buttons[i].id,
+                "event": element.id,
             }
             socket.send(JSON.stringify(data));
         });
     }
+}
+
+function scoringButtonSetup() {
+    const homeScoreButton = document.getElementById("home-score");
+    const awayScoreButton = document.getElementById("away-score");
+
+    function toggleButton(button, team) {
+        const playerButtonsContainer = document.getElementById(team === "home" ? "Aanval" : "Verdediging");
+        const playerButtons = playerButtonsContainer.getElementsByClassName("player-selector");
+    
+        if (button.classList.contains("activated")) {
+            // Deactivate the button if it's already activated
+            button.style.background = team === "home" ? "#43ff6480" : "rgba(235, 0, 0, 0.5)";
+            button.classList.remove("activated");
+    
+            // Remove event listeners from the deactivated button
+            Array.from(playerButtons).forEach(element => {
+                element.style.background = "";
+                if (element._playerClickHandler) {
+                    element.removeEventListener("click", element._playerClickHandler);
+                    delete element._playerClickHandler;
+                }
+            });
+        } else {
+            // First, deactivate the currently activated button (if any)
+            const activatedButton = document.querySelector(".activated");
+            if (activatedButton) {
+                const otherTeam = activatedButton === homeScoreButton ? "home" : "away";
+                activatedButton.style.background = otherTeam === "home" ? "#43ff6480" : "rgba(235, 0, 0, 0.5)";
+                activatedButton.classList.remove("activated");
+    
+                // Remove event listeners from the previously activated button
+                const otherPlayerButtonsContainer = document.getElementById(otherTeam === "home" ? "Aanval" : "Verdediging");
+                const otherPlayerButtons = otherPlayerButtonsContainer.getElementsByClassName("player-selector");
+                Array.from(otherPlayerButtons).forEach(element => {
+                    element.style.background = "";
+                    if (element._playerClickHandler) {
+                        element.removeEventListener("click", element._playerClickHandler);
+                        delete element._playerClickHandler;
+                    }
+                });
+            }
+    
+            // Activate the pressed button
+            button.style.background = team === "home" ? "#43ff64" : "rgba(235, 0, 0, 0.7)";
+            button.classList.add("activated");
+    
+            // Apply changes and add event listeners to the pressed button
+            Array.from(playerButtons).forEach(element => {
+                element.style.background = team === "home" ? "#43ff6480" : "rgba(235, 0, 0, 0.5)";
+    
+                const playerClickHandler = function () {
+                    console.log("Player clicked: " + element.id);
+    
+                    const data = {
+                        "command": "score",
+                        "team": team,
+                        "player": element.id,
+                    }
+                    socket.send(JSON.stringify(data));
+                };
+    
+                element._playerClickHandler = playerClickHandler;
+                element.addEventListener("click", playerClickHandler);
+            });
+        }
+    }    
+
+    homeScoreButton.addEventListener("click", function () {
+        toggleButton(homeScoreButton, "home");
+    });
+
+    awayScoreButton.addEventListener("click", function () {
+        toggleButton(awayScoreButton, "away");
+    });
 }
 
 function requestInitalData() {
@@ -118,6 +194,7 @@ function cleanDom(element) {
 }
 
 function showPlayerGroups(data) {
+    let switchButton = false;
     const playerGroups = data.playerGroups;
 
     const playerGroupContainer = document.createElement("div");
@@ -133,10 +210,9 @@ function showPlayerGroups(data) {
             const playerGroupTitleDiv = document.createElement("div");
             playerGroupTitleDiv.classList.add("flex-row");
             playerGroupTitleDiv.classList.add("player-group-title");
-            playerGroupTitleDiv.style.justifyContent = "flex-start";
             playerGroupTitleDiv.style.marginBottom = "6px";
-            playerGroupTitleDiv.style.marginLeft = "12px";
-            playerGroupTitleDiv.style.width = "calc(100% - 12px)";
+            playerGroupTitleDiv.style.margin = "0 12px 6px 12px";
+            playerGroupTitleDiv.style.width = "calc(100% - 24px)";
 
             const playerGroupTitle = document.createElement("div");
             playerGroupTitle.style.fontWeight = "600";
@@ -145,10 +221,16 @@ function showPlayerGroups(data) {
 
             playerGroupTitleDiv.appendChild(playerGroupTitle);
 
-            if (!document.getElementById("switch-button")) {
-                const switchButton = document.createElement("div");
-                switchButton.innerHTML = "Wissel";
-                switchButton.addEventListener("click", function() {
+            if (!switchButton) {
+                const switchButtonDiv = document.createElement("div");
+                switchButtonDiv.innerHTML = "Wissel";
+                switchButtonDiv.id = "switch-button";
+
+                switchButtonDiv.classList.add("switch-button");
+                switchButtonDiv.classList.add("flex-center");
+                switchButtonDiv.style.width = "96px";
+
+                switchButtonDiv.addEventListener("click", function() {
                     const data = {
                         "command": "switch",
                         "player_group": playerGroup.id,
@@ -156,7 +238,9 @@ function showPlayerGroups(data) {
                     socket.send(JSON.stringify(data));
                 });
 
-                playerGroupTitleDiv.appendChild(switchButton);
+                playerGroupTitleDiv.appendChild(switchButtonDiv);
+
+                switchButton = true;
             }
 
             const playerGroupPlayers = document.createElement("div");
@@ -164,6 +248,8 @@ function showPlayerGroups(data) {
             playerGroupPlayers.classList.add("flex-row");
             playerGroupPlayers.style.flexWrap = "wrap";
             playerGroupPlayers.style.alignItems = 'stretch';
+
+            playerGroupPlayers.id = playerGroup.starting_type;
         
             for (let i = 0; i < 4; i++) {
                 let player = playerGroup.players[i];
@@ -189,7 +275,7 @@ function showPlayerGroups(data) {
                 playerGroupPlayers.appendChild(playerDiv);
             }
 
-            playerGroupDiv.appendChild(playerGroupTitle);
+            playerGroupDiv.appendChild(playerGroupTitleDiv);
             playerGroupDiv.appendChild(playerGroupPlayers);
 
             playerGroupContainer.appendChild(playerGroupDiv);
