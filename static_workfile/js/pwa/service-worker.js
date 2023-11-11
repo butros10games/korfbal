8624e33dac515b2b4af2cdf200742cd0c22668fe
Butros Groot Desktop <butrosgroot@gmail.com>
2023-11-11 11:40:23 +0100

    Add PWA manifest and service worker

---
+++ static_workfile/js/pwa/service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener('fetch', function(event) {
+    // This is a minimal no-op service worker.
+});
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/static/service-worker.js').then(function(registration) {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
