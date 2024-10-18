927f5afcb1f928d63381324fd77c54c6f1bd7add
Butros Groot MacBook <butrosgroot@gmail.com>
2024-10-18 11:05:35 +0200

    # game tracker consumers
    ## Update
    - Split up the consumers in multiple files to make it more readable

---
+++ apps/game_tracker/consumers/__init__.py
@@ -0,0 +1,7 @@
+from .match_data import match_data
+from .match_tracker import match_tracker
+
+__all__ = [
+    'match_data',
+    'match_tracker'
+]
---
+++ apps/game_tracker/consumers/common.py
@@ -0,0 +1,48 @@
+from asgiref.sync import sync_to_async
+
+from apps.game_tracker.models import MatchPart, Pause
+
+import json
+
+async def get_time(match_data, current_part):
+    # check if there is a active part if there is a active part send the start time of the part and lenght of a match part
+    try:
+        part = await MatchPart.objects.aget(match_data=match_data, active=True)
+    except MatchPart.DoesNotExist:
+        part = False
+        
+    if part:
+        # check if there is a active pause if there is a active pause send the start time of the pause
+        try:
+            active_pause = await Pause.objects.aget(match_data=match_data, active=True, match_part=current_part)
+        except Pause.DoesNotExist:
+            active_pause = False
+        
+        # calculate all the time in pauses that are not active anymore
+        pauses = await sync_to_async(list)(Pause.objects.filter(match_data=match_data, active=False, match_part=current_part))
+        pause_time = 0
+        for pause in pauses:
+            pause_time += pause.length().total_seconds()
+        
+        if active_pause:
+            return json.dumps({
+                'command': 'timer_data',
+                'type': 'pause',
+                'time': part.start_time.isoformat(),
+                'calc_to': active_pause.start_time.isoformat(),
+                'length': match_data.part_lenght,
+                'pause_length': pause_time
+            })
+        else:
+            return json.dumps({
+                'command': 'timer_data',
+                'type': 'active',
+                'time': part.start_time.isoformat(),
+                'length': match_data.part_lenght,
+                'pause_length': pause_time
+            })
+    else:
+        return json.dumps({
+            'command': 'timer_data',
+            'type': 'deactive'
+        })
