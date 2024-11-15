4458e69962f0785d1ece48a33b13fb4e6c80c204
Butros Groot Desktop <butrosgroot@gmail.com>
2024-11-15 11:54:52 +0100

    # General
    ## Bugfix
    - asig import order fix
    - pre-commit fix

--- Korfbal/asgi.py
+++ Korfbal/asgi.py
@@ -3,14 +3,17 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-from apps.club.routing import websocket_urlpatterns as club_routing
-from apps.game_tracker.routing import websocket_urlpatterns as game_tracker_routing
-from apps.player.routing import websocket_urlpatterns as player_routing
-from apps.team.routing import websocket_urlpatterns as team_routing
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Korfbal.settings")
 django_asgi_app = get_asgi_application()
+
+from apps.club.routing import websocket_urlpatterns as club_routing  # noqa: E402
+from apps.game_tracker.routing import (  # noqa: E402, E501
+    websocket_urlpatterns as game_tracker_routing,
+)
+from apps.player.routing import websocket_urlpatterns as player_routing  # noqa: E402
+from apps.team.routing import websocket_urlpatterns as team_routing  # noqa: E402
 
 all_websocket_urlpatterns = (
     game_tracker_routing + club_routing + player_routing + team_routing
