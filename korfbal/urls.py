cce3407b0980bc5209c1834b3e8215f8f5444983
Butros Groot Desktop <butrosgroot@gmail.com>
2024-11-27 17:46:36 +0100

    # Korfbal folder decapatalisation

---
+++ korfbal/urls.py
@@ -0,0 +1,14 @@
+"""korfbal URL Configuration."""
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("match/", include("apps.game_tracker.urls")),
+    path("club/", include("apps.club.urls")),
+    path("profile/", include("apps.player.urls")),
+    path("teams/", include("apps.team.urls")),
+    path("", include("apps.hub.urls")),
+    path("", include("authentication.urls")),
+]
